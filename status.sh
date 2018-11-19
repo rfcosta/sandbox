1a9ff3a0a0c77497146b095bfa9533c0f5a19b8f
Reginaldo Costa <Reginaldo.Costa@sabre.com>
2018-11-18 20:06:19 -0600

    NOV 18 20:06

--- status.sh
+++ status.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 
-# ....+....1....+....2....+....3....+....4....+....5....+....6....+....7....+....8....+....9....+...10....+...11....+...12....+...13....+...14....+
-# CONTAINER ID        IMAGE               COMMAND             CREATED             STATUS                      PORTS               NAMES
-# be5bc112767f        hello-world         "/hello"            5 weeks ago         Exited (0) 5 weeks ago                          flamboyant_jang
-# 64af8e775944        centos:regi         "/bin/bash"         7 months ago        Exited (127) 7 months ago                       quirky_mcnulty
-
-# 1-13                21-40               41-60               61-80               81-108                      109-128             129-145
-
-ID="1-13"
-IMAGE="21-40"
-COMMAND="41-60"
-CREATED="61-80"
-STATUS="81-108"
-PORTS="109-128"
-NAMES="129-145"
-
-docker ps -a | cut -c $ID,$IMAGE,$COMMAND,$STATUS,$NAMES -
+docker ps -a | awk -v WITDH=145 '{print substr($0,1,WITDH)}'
 
 
 
+# CONTAINER ID        IMAGE                      COMMAND                  CREATED              STATUS                      PORTS                                                                    NAMES
+# 3d34c7798eab        grafana                    "/run.sh"                About a minute ago   Up About a minute           0.0.0.0:3000->3000/tcp                                                   influxdata_grafana_1
+# 0420a768e8fa        chrono_config              "/entrypoint.sh chro…"   About a minute ago   Up About a minute           0.0.0.0:8888->8888/tcp                                                   influxdata_chronograf_1
+# 3f36c0d32054        kapacitor                  "/entrypoint.sh kapa…"   About a minute ago   Up About a minute           0.0.0.0:9092->9092/tcp                                                   influxdata_kapacitor_1
+# 3e777738e16d        telegraf                   "/entrypoint.sh tele…"   About a minute ago   Up About a minute           8092/udp, 8125/udp, 8094/tcp                                             influxdata_telegraf_1
+# e45c53692801        influxdb                   "/entrypoint.sh infl…"   About a minute ago   Up About a minute           0.0.0.0:8082->8082/tcp, 0.0.0.0:8086->8086/tcp, 0.0.0.0:8089->8089/tcp   influxdata_influxdb_1
+# b54274eea091        influxdata_documentation   "/documentation/docu…"   About a minute ago   Up About a minute           0.0.0.0:3010->3000/tcp                                                   influxdata_documentation_1
+# be5bc112767f        hello-world                "/hello"                 5 weeks ago          Exited (0) 5 weeks ago                                                                               flamboyant_jang
+# 64af8e775944        centos:regi                "/bin/bash"              7 months ago         Exited (127) 7 months ago                                                                            quirky_mcnulty
