b9441473df164f8e66aaa44879d1c50122ba53e1
Costa, Reginaldo <Reginaldo.Costa@sabre.com>
2019-07-12 16:52:17 -0500

    Work on data loader to load data on this InfluxDB stack * IN PROGRESS **

--- grafana/home/centos/loadData.py
+++ grafana/home/centos/loadData.py
@@ -47,66 +47,68 @@ def LoadJson(json_filename):
 
 
 
-parser = OptionParser()
-parser.add_option("-data_file", dest="metrics_file", default="", help="Input data file",   required=False)
-parser.add_option("-options",   dest="options_file", default="", help="Options json file", required=True)
-parser.add_option("-service",   dest="service"     , default="", help="Service",           required=False)
-parser.add_option("-key",       dest="key"         , default="", help="Key",               required=False)
-parser.add_option("-metric",    dest="metric"      , default="", help="Metric",            required=False)
-parser.add_option("-source",    dest="source"      , default="", help="Data Source",       required=False)
+if __name__ == "__main__":
+
+    parser = OptionParser()
+    parser.add_option("--csv",       dest="csv"         , default="", help="Input data file",   )
+    parser.add_option("--options",   dest="options_file", default="", help="Options json file"  )
+    parser.add_option("--service",   dest="service"     , default="", help="Service"            )
+    parser.add_option("--key",       dest="key"         , default="", help="Key"                )
+    parser.add_option("--metric",    dest="metric"      , default="", help="Metric"             )
+    parser.add_option("--source",    dest="source"      , default="", help="Data Source"        )
 
 
-(options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
-options_file = options.options_file
-OPTIONS = LoadJson(options_file)
+    options_file = options.options_file
+    OPTIONS = LoadJson(options_file)
 
-parameterNames = ["metrics_file", "service", "key", "metric", "source"]
+    parameterNames = ["csv", "service", "key", "metric", "source"]
 
-for parm in parameterNames:
-    value = options.get(parm)
-    if value:
-        OPTIONS[parm] = value
+    for parm in parameterNames:
+        value = options[parm]
+        if value:
+            OPTIONS[parm] = value
 
-print "OPTIONS: " + json.dumps(OPTIONS, indent=4)
+    print "OPTIONS: " + json.dumps(OPTIONS, indent=4)
 
-# (service, key, metric) = metrics_file.split('^', 3)
-# metric = metric.replace(".csv", "")
+    # (service, key, metric) = metrics_file.split('^', 3)
+    # metric = metric.replace(".csv", "")
 
-type = OPTIONS.get("metric")
-metrics_file = OPTIONS.get("metrics_file")
-service      = OPTIONS.get("service")
-key          = OPTIONS.get("key")
-metric       = OPTIONS.get("metric")
-source       = OPTIONS.get("source") # Sources "AppDynamics" | "VIZ" | "Zabbix" | "Service Supplied"
+    type         = OPTIONS.get("metric")
+    metrics_file = OPTIONS.get("csv")
+    service      = OPTIONS.get("service")
+    key          = OPTIONS.get("key")
+    metric       = OPTIONS.get("metric")
+    source       = OPTIONS.get("source") # Sources "AppDynamics" | "VIZ" | "Zabbix" | "Service Supplied"
 
-influx_url = "http://localhost:8086/write?db=kpi"
+    influx_url = "http://localhost:8086/write?db=kpi"
 
-first_line = 1
+    first_line = 1
 
-size = os.stat(metrics_file).st_size
-print "FILE: " + metrics_file + ", Size: " + str(size)
+    size = os.stat(metrics_file).st_size
+    print "FILE: " + metrics_file + ", Size: " + str(size)
 
-if (size > 100):
-    with open(metrics_file) as f:
-        for line in f:
-            print line
-            if first_line == 1:
-                first_line = 0
-                continue
-            line = line.rstrip()
+    if (size > 100):
+        with open(metrics_file) as f:
+            for line in f:
+                print line
+                if first_line == 1:
+                    first_line = 0
+                    continue
+                line = line.rstrip()
 
-            (when, service, count) = line.split(',', 3)
-            print "SERVICE: " + service
-            service = encode(service)
-            when = str(convert_utc_to_epoch(when))
-            print "Type: " + str(type)
-            tags = "ci=" + service + ",key=" + key + ",source=" + source + ",type=" + metric
-            values = type + "=" + count
-            data = "metric," + tags + " " + values + " " + when
-            print "DATA: " + data
+                (when, service, count) = line.split(',', 3)
+                print "SERVICE: " + service
+                service = encode(service)
+                when = str(convert_utc_to_epoch(when))
+                print "Type: " + str(type)
+                tags = "ci=" + service + ",key=" + key + ",source=" + source + ",type=" + metric
+                values = type + "=" + count
+                data = "metric," + tags + " " + values + " " + when
+                print "DATA: " + data
 
-            command = "curl -s -i -XPOST " + influx_url + " --data-binary \"" + data + "\""
-            print command
-            os.system(command)
+                command = "curl -s -i -XPOST " + influx_url + " --data-binary \"" + data + "\""
+                print command
+                os.system(command)
 
