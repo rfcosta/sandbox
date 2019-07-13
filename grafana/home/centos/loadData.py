50e4338d5beda24e0f8d9d45999baa9688970b53
Reginaldo Costa <Reginaldo.Costa@sabre.com>
2019-07-13 13:51:58 -0500

    perfect data loader

--- grafana/home/centos/loadData.py
+++ grafana/home/centos/loadData.py
@@ -49,48 +49,58 @@ def LoadJson(json_filename):
 
 if __name__ == "__main__":
 
-    parser = OptionParser()
-    parser.add_option("-f",   "--file",      dest="csv"         , default="", help="Input data file",   )
-    parser.add_option("-o",   "--options",   dest="options_file", default="", help="Options json file"  )
-    parser.add_option("-v",   "--service",   dest="service"     , default="", help="Service"            )
-    parser.add_option("-k",   "--key",       dest="key"         , default="", help="Key"                )
-    parser.add_option("-m",   "--metric",    dest="metric"      , default="", help="Metric"             )
-    parser.add_option("-s",   "--source",    dest="source"      , default="", help="Data Source"        )
+    parser = OptionParser(add_help_option=False)
+    parser.add_option("-h",   "--help",      action="help")
+    parser.add_option("-f",   "--file",      dest="csv"         , help="Input data file"  , default='')
+    parser.add_option("-o",   "--options",   dest="options_file", help="Options json file", default='')
+    parser.add_option("-v",   "--service",   dest="service"     , help="Service"          , default='')
+    parser.add_option("-k",   "--key",       dest="key"         , help="Key"              , default='')
+    parser.add_option("-m",   "--metric",    dest="metric"      , help="Metric"           , default='')
+    parser.add_option("-s",   "--source",    dest="source",
+                         help='Data Source (AppDynamics | VIZ | Zabbix | Service Supplied)',
+                         type="choice", choices=["AppDynamics", "VIZ", "Zabbix", "Service Supplied", ""], default=''
+                     )
+    parser.add_option("-u",   "--url",       dest="url"         , help="Influx URL"       , default='http://localhost:8086/write?db=kpi')
 
 
     (options, args) = parser.parse_args()
 
+    # print( options.__dict__.keys() )
+
+
+    # ----------------------------------------------------------------------------------------------------------
+    # The following statement is just to ilustrate how to address the attributes of an object like a dictionary
+    #       options.__dict__['source'] = 'VIZ' if not options.source else options.source
+    # ----------------------------------------------------------------------------------------------------------
+
     options_file = options.options_file
-    OPTIONS = LoadJson(options_file)
+    options_from_json = LoadJson(options_file)
 
-    parameterNames = ["csv", "service", "key", "metric", "source"]
+    # Now get overrides from options into the options from JSON (this will prioritize non null options
+    options_from_json.update(  (ky, val) for (ky,val) in options.__dict__.iteritems() if val  )
 
-    for parm in parameterNames:
-        value = options[parm]
-        if value:
-            OPTIONS[parm] = value
+    #update back options after the overides
+    options.__dict__.update(options_from_json)
 
-    print "OPTIONS: " + json.dumps(OPTIONS, indent=4)
+    print("Options: " + str(options))
 
-    # (service, key, metric) = metrics_file.split('^', 3)
-    # metric = metric.replace(".csv", "")
+    # ----------------------------------------------------------------------------------------------------------
+    # The following code used to use filename with service, key and metric within the file name
+    # Deprecated by the new program options and json options file
+    # ----------------------------------------------------------------------------------------------------------
+    #       (service, key, metric) = metrics_file.split('^', 3)
+    #       metric = metric.replace(".csv", "")
+    # ----------------------------------------------------------------------------------------------------------
 
-    type         = OPTIONS.get("metric")
-    metrics_file = OPTIONS.get("csv")
-    service      = OPTIONS.get("service")
-    key          = OPTIONS.get("key")
-    metric       = OPTIONS.get("metric")
-    source       = OPTIONS.get("source") # Sources "AppDynamics" | "VIZ" | "Zabbix" | "Service Supplied"
-
-    influx_url = "http://localhost:8086/write?db=kpi"
+    influx_url = options.url # "http://localhost:8086/write?db=kpi"
 
     first_line = 1
 
-    size = os.stat(metrics_file).st_size
-    print "FILE: " + metrics_file + ", Size: " + str(size)
+    size = os.stat(options.csv).st_size
+    print "FILE: " + options.csv + ", Size: " + str(size)
 
     if (size > 100):
-        with open(metrics_file) as f:
+        with open(options.csv) as f:
             for line in f:
                 print line
                 if first_line == 1:
@@ -99,13 +109,14 @@ if __name__ == "__main__":
                 line = line.rstrip()
 
                 (when, service, count) = line.split(',', 3)
-                print "SERVICE: " + service
-                service = encode(service)
+                service = encode(options.service)
                 when = str(convert_utc_to_epoch(when))
-                print "Type: " + str(type)
-                tags = "ci=" + service + ",key=" + key + ",source=" + source + ",type=" + metric
-                values = type + "=" + count
+                tags = "ci=" + service + ",key=" + options.key + ",source=" + options.source + ",type=" + options.metric
+                values = options.metric + "=" + count
                 data = "metric," + tags + " " + values + " " + when
+
+                print "SERVICE: " + service
+                print "Type: " + str(options.metric)
                 print "DATA: " + data
 
                 command = "curl -s -i -XPOST " + influx_url + " --data-binary \"" + data + "\""
