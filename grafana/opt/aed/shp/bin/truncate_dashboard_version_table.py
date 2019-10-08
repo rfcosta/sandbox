94cc4abd2940d744ead8627dbed275b78cc1fb6c
Costa, Reginaldo <Reginaldo.Costa@sabre.com>
2019-10-08 16:03:47 -0500

    Change package install for grafana to Alpine add user parameter to "sandbox enter" command

---
+++ grafana/opt/aed/shp/bin/truncate_dashboard_version_table.py
@@ -0,0 +1,27 @@
+import sys
+import mysql.connector
+
+sys.path.append('/opt/aed/shp/lib')
+
+import shputil
+
+config = shputil.get_config()
+
+mysql_host = config['mysql_host']
+mysql_user = config['mysql_user']
+mysql_password = config['mysql_password']
+mysql_db_name = config['mysql_db_name']
+
+db = mysql.connector.connect(
+  host=mysql_host,
+  user=mysql_user,
+  passwd=mysql_password,
+  database=mysql_db_name
+)
+
+cursor= db.cursor()
+
+cursor.execute("TRUNCATE TABLE dashboard_version")
+cursor.execute("OPTIMIZE TABLE dashboard_version")
+
+db.close()
